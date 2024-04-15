71049ae8efd76ae7621136df82468d3975be3cf8
cristiangligan <cristian.gligan@gmail.com>
2024-04-15 13:39:05 +0200

    Sign In / Sign Up selector screen logic - created action listener method on sign in button

---
+++ QuizManiaGUI/src/Controller.java
@@ -0,0 +1,9 @@
+import view.FlashcardSetsFrame;
+import view.FlashcardsFrame;
+
+public class Controller {
+    public static void main(String[] args) {
+        FlashcardSetsFrame flashcardSetsFrame = new FlashcardSetsFrame();
+        FlashcardsFrame flashcardsFrame = new FlashcardsFrame();
+    }
+}
